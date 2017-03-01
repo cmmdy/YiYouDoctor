d145864dcad8fe3c3c9162a0f61f1362513480a1
cmmdy <cmmdy@gmail.com>
2017-03-02 00:18:24 +0800

    3.2

---
+++ app/src/main/java/com/example/yiyoudoctor/HomeActicity.java
@@ -0,0 +1,214 @@
+package com.example.yiyoudoctor;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
+
+
+
+public class HomeActicity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+
+    private double WIDTH;
+    private double HEIGHT;
+
+    private Toolbar toolbar;
+
+
+    /**
+     * ViewPager
+     */
+    private ViewPager viewPager;
+
+    /**
+     * 装点点的ImageView数组
+     */
+    private ImageView[] tips;
+
+    /**
+     * 装ImageView数组
+     */
+    private ImageView[] mImageViews;
+
+    /**
+     * 图片资源id
+     */
+    private int[] imgIdArray ;
+
+    private boolean isLooper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //获得屏幕大小
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        WIDTH = dm.widthPixels;
+        HEIGHT = dm.heightPixels;
+
+        setContentView(R.layout.activity_home_acticity);
+        //初始化布局
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+
+        ViewGroup group = (ViewGroup)findViewById(R.id.viewGroup);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        //载入图片资源ID
+        imgIdArray = new int[]{R.drawable.viewpager1, R.drawable.viewpager2, R.drawable.viewpager3, R.drawable.viewpager4,};
+
+        FrameLayout point_layout = (FrameLayout) findViewById(R.id.point_layout);
+        point_layout.getLayoutParams().height = (int) (HEIGHT / 35);
+
+        //将点点加入到ViewGroup中
+        tips = new ImageView[imgIdArray.length];
+        for(int i=0; i<tips.length; i++){
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(10,10));
+            tips[i] = imageView;
+            if(i == 0){
+                tips[i].setBackgroundResource(R.drawable.bullet_yellow);
+            }else{
+                tips[i].setBackgroundResource(R.drawable.bullet_white);
+            }
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(70,70);
+            layoutParams.leftMargin = 5;
+            layoutParams.rightMargin = 5;
+            group.addView(imageView, layoutParams);
+        }
+
+
+        //将图片装载到数组中
+        mImageViews = new ImageView[imgIdArray.length];
+        for(int i=0; i<mImageViews.length; i++){
+            ImageView imageView = new ImageView(this);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            mImageViews[i] = imageView;
+            Glide.with(this).load(imgIdArray[i]).into(imageView);
+        }
+
+        //设置Adapter
+        viewPager.setAdapter(new MyAdapter());
+        //设置监听，主要是设置点点的背景
+        viewPager.setOnPageChangeListener(this);
+        //设置ViewPager的默认项, 设置为长度的100倍，这样子开始就能往左滑动
+        viewPager.setCurrentItem((mImageViews.length) * 100);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                isLooper = true;
+                while (isLooper){
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //这里是设置当前页的下一页
+                            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                        }
+                    });
+                }
+            }
+        }).start();
+
+//        viewPager.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                isLooper = false;
+//            }
+//        });
+
+    }
+
+    //创建菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+
+
+    public class MyAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return Integer.MAX_VALUE;
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public void destroyItem(View container, int position, Object object) {
+//            int newPosition = position % mImageViews.length;
+//            container.((View)mImageViews[newPosition]);
+        }
+
+        /**
+         * 载入图片进去，用当前的position 除以 图片数组长度取余数是关键
+         */
+        @Override
+        public Object instantiateItem(View container, int position) {
+            try {
+                ((ViewPager)container).addView(mImageViews[position % mImageViews.length], 0);
+            }catch(Exception e){
+                //handler something
+            }
+            return mImageViews[position % mImageViews.length];
+        }
+
+
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int arg0) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+    }
+
+    @Override
+    public void onPageSelected(int arg0) {
+        setImageBackground(arg0 % mImageViews.length);
+    }
+
+    /**
+     * 设置选中的tip的背景
+     * @param selectItems
+     */
+    private void setImageBackground(int selectItems){
+        for(int i=0; i<tips.length; i++){
+            if(i == selectItems){
+                tips[i].setBackgroundResource(R.drawable.bullet_yellow);
+            }else{
+                tips[i].setBackgroundResource(R.drawable.bullet_white);
+            }
+        }
+    }
+
+
+}
